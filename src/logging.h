c8d1493a17891605b40cb52a86ce039f263a778f
Michael Lynch <lyncmi07@yahoo.co.uk>
2020-01-18 22:59:48 +0000

    Using preprocessor implementation

--- src/logging.h
+++ src/logging.h
@@ -2,15 +2,22 @@
 #define LOGGING_H
 
 #include <iostream>
-#include <string>
 
-namespace log {
-inline void println(std::string message) {
-    (void)(message);
-#ifdef DEBUG
-    std::cout << message << std::endl;
+#if defined(DEBUG)
+#define DEBUG_LOG(x) std::cout << x << std::endl
+#else
+#define DEBUG_LOG(x) {}
 #endif
-}
-}
 
+#if defined(INFO) || defined(DEBUG)
+#define INFO_LOG(x) std::cout << x << std::endl
+#else
+#define INFO_LOG(x) {}
+#endif
+
+#if defined(ERROR) || defined(INFO) || defined(DEBUG)
+#define ERROR_LOG(x) std::cout << x << std::endl
+#else
+#define ERROR_LOG(x) {}
+#endif
 #endif
