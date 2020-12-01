a190d6ae3f41982ce47af1f940df56e2aa85ca3d
Michael Lynch <michael@supermarinesoftware.com>
2020-12-01 01:01:38 +0000

    Add string quote end

--- include/logging.h
+++ include/logging.h
@@ -5,7 +5,7 @@
     #include <iostream>
 
     #if defined(DEBUG)
-        #define DEBUG_LOG(x) std::cout << "DEBUG: << x << std::endl
+        #define DEBUG_LOG(x) std::cout << "DEBUG:" << x << std::endl
     #else
         #define DEBUG_LOG(x) {}
     #endif
